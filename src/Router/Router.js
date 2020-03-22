5b8a06abcf3fc88cc1fd3f47dd96b69360977756
yurkagon <owngameplay@gmail.com>
2020-03-22 17:21:01 +0200

    [ADD] task page

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -3,13 +3,13 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import DashboardRoute from "./DashboardRoute";
 
-import { Home } from "../pages";
+import { Home, Task } from "../pages";
 
 const RouteSchema = () => (
   <Router>
     <Switch>
       <DashboardRoute exact path="/" component={Home} />
-      <DashboardRoute exact path="/test" component={() => "Марішка"} />
+      <DashboardRoute exact path="/task" component={Task} />
 
       <Route exact component={() => <h1 className="text-center"> 404 </h1>} />
     </Switch>

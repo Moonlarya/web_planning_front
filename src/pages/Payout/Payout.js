5db6d1b8eb6b2dc11b6919449fa5288fb5299072
Moonlarya <gum225@gmail.com>
2020-04-23 03:07:20 +0300

    finded calendar and added to payout for example

--- src/pages/Payout/Payout.js
+++ src/pages/Payout/Payout.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
-const Payout = () => {
-  return (
-    <main class="col-8 bonuce">
-      <h5>История начислений</h5>
-      <div id="your-id" style={{ height: 400 }}></div>
-    </main>
-  );
-};
+class Payout extends Component {
+  state = {
+    date: new Date(),
+  };
+
+  onChange = (date) => this.setState({ date });
+
+  render() {
+    return (
+      <div>
+        <main class="col-8 bonuce">
+          <h5>История начислений</h5>
+          <div id="your-id" style={{ height: 400 }}></div>
+        </main>
+        <Calendar onChange={this.onChange} value={this.state.date} />
+      </div>
+    );
+  }
+}
 
 export default Payout;

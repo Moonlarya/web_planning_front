c14a289412205ddb6467bcd33b23a920a531abe7
yurkagon <owngameplay@gmail.com>
2020-06-06 03:47:35 +0300

    [FIX] nodemon

--- src/components/EmployeeGrades/EmployeeGrades.js
+++ src/components/EmployeeGrades/EmployeeGrades.js
@@ -11,40 +11,37 @@ class EmployeeGrades extends Component {
 
   componentDidMount = async () => {
     const { employeeId } = this.props.data[0];
+
     const employee = await EmployeesService.get(employeeId);
+
     this.setState({ employee });
   };
 
   getCriteriaById = (id) => {
-    return this.props.criterias.find((el) => el._id === id);
+    const { criterias } = this.props;
+
+    return criterias.find((el) => el._id === id);
   };
 
   render() {
     const { data } = this.props;
     const { employee } = this.state;
+
     if (!employee) {
       return null;
     }
 
+    console.log(data);
     return (
       <div className="card col-3">
-        <h4>
-          Имя сотрудника
-          {/*`${employee.surname} ${employee.name} ${employee.patronymic}`*/}
-        </h4>
-        <p>Должность: дизайнер {/*positionTypes[employee.type]*/}</p>
+        <h4>{`${employee.surname} ${employee.name} ${employee.patronymic}`}</h4>
+        <p>Должность: {positionTypes[employee.type]}</p>
         <ul className="list-group">
           <li className="list-group-item">
             <p>Дата 1</p>
             <p className="text-left">Критерий 1: оценка</p>
             <p className="text-left">Оптимистичность : 3</p>
             <p className="text-left">Продуктивность : 4</p>
-          </li>
-          <li className="list-group-item">
-            <p>Дата 2</p>
-            <p className="text-left">Критерий 1: оценка</p>
-            <p className="text-left">Оптимистичность : 1</p>
-            <p className="text-left">Продуктивность : 5</p>
           </li>
           {/*data.map((element) => {
             const criteria = this.getCriteriaById(element.criteriaId);

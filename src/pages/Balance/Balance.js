5356b493f918df62f4fc14f78404e3b781233d06
yurkagon <owngameplay@gmail.com>
2020-06-11 05:14:19 +0300

    Added a piece of magic

--- src/pages/Balance/Balance.js
+++ src/pages/Balance/Balance.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import moment from "moment";
+import _ from "lodash";
+
 import TaskService from "../../services/TasksService";
+import Chart from "./Chart";
 
 import { withAuth } from "../../stores/User";
 
@@ -58,6 +61,12 @@ class Balance extends Component {
     const { bonuceSum, fineSum, available, final } = this.getBalanceData();
 
     const weekFormat = "Do MMMM YYYY";
+
+    const finishedTasks = _.filter(tasks, { status: "finished" });
+    const tasksByWeek = _.filter(finishedTasks, (el) =>
+      moment(el.finishDate).isSame(week, "week")
+    );
+
     return (
       <div>
         <section className="d-flex align-items-start flex-wrap">
@@ -72,8 +81,8 @@ class Balance extends Component {
           </div>
         </section>
 
-        <section>
-          <div className="d-flex justify-content-center align-items-center">
+        <section className="d-flex flex-column align-items-center">
+          <div className="d-flex justify-content-center align-items-center mb-3">
             <button
               className="btn btn-primary m-1"
               onClick={() => this.handleWeekChange(-1)}
@@ -91,77 +100,46 @@ class Balance extends Component {
               {">"}
             </button>
           </div>
+
+          <Chart tasks={tasksByWeek} />
         </section>
 
         <section>
           <h5 className="mx-auto">Бонусы</h5>
           <div className="d-flex align-items-start flex-wrap">
-            {tasks.map((task) => {
-              if (task.status === "finished")
-                return (
-                  <div className="card col-3 text-left" key={task._id}>
-                    <div className="card-body">
-                      <h5 className="card-title card-header">{task.name}</h5>
+            {tasksByWeek.map((task) => (
+              <div className="card col-3 text-left" key={task._id}>
+                <div className="card-body">
+                  <h5 className="card-title card-header">{task.name}</h5>
+                  <p className="card-title">
+                    Дедлайн: {moment(`${task.deadline}`).format("Do MMMM YYYY")}
+                  </p>
+                  <p className="card-text">
+                    Дата завершения:
+                    {moment(task.finishDate).format("Do MMMM YYYY")}
+                  </p>
+                  {task.employee &&
+                    typeof task.employee !== "string" &&
+                    this.props.user.type === "manager" && (
                       <p className="card-title">
-                        Дедлайн:{" "}
-                        {moment(`${task.deadline}`).format("Do MMMM YYYY")}
+                        Исполнитель:
+                        {`${task.employee.surname} ${task.employee.name} ${task.employee.patronymic}`}
                       </p>
-                      <p className="card-text">
-                        Дата завершения:
-                        {moment(task.finishDate).format("Do MMMM YYYY")}
-                      </p>
-                      {task.employee &&
-                        typeof task.employee !== "string" &&
-                        this.props.user.type === "manager" && (
-                          <p className="card-title">
-                            Исполнитель:
-                            {`${task.employee.surname} ${task.employee.name} ${task.employee.patronymic}`}
-                          </p>
-                        )}
-                      {task.project && (
-                        <p className="card-title">
-                          Проект: {task.project.name}
-                        </p>
-                      )}
-                      <p className="card-title">
-                        Количество бонусов: {task.bonuce}
-                      </p>
-                      {task.earnedBonuce && (
-                        <p className="card-text">
-                          Полученные бонусы: {task.earnedBonuce.toFixed(2)}
-                        </p>
-                      )}
-                    </div>
-                  </div>
-                );
-              else
-                return (
-                  <div className="card col-3" key={task._id}>
-                    <div className="card-body text-left">
-                      <h5 className="card-header">{task.name}</h5>
-                      <p className="card-title">
-                        Дедлайн:{" "}
-                        {moment(`${task.deadline}`).format("Do MMMM YYYY")}
-                      </p>
-
-                      {task.employee && typeof task.employee !== "string" && (
-                        <p className="card-title">
-                          Исполнитель:
-                          {`${task.employee.surname} ${task.employee.name} ${task.employee.patronymic}`}
-                        </p>
-                      )}
-                      {task.project && (
-                        <p className="card-title">
-                          Проект: {task.project.name}
-                        </p>
-                      )}
-                      <p className="card-title">
-                        Количество бонусов: {task.bonuce}
-                      </p>
-                    </div>
-                  </div>
-                );
-            })}
+                    )}
+                  {task.project && (
+                    <p className="card-title">Проект: {task.project.name}</p>
+                  )}
+                  <p className="card-title">
+                    Количество бонусов: {task.bonuce}
+                  </p>
+                  {task.earnedBonuce && (
+                    <p className="card-text">
+                      Полученные бонусы: {task.earnedBonuce.toFixed(2)}
+                    </p>
+                  )}
+                </div>
+              </div>
+            ))}
           </div>
         </section>
       </div>
